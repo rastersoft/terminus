d486810576dc29c6000ae9f038e4c894d4c49012
Sergio Costas <raster@rastersoft.com>
2017-09-18 13:01:51 +0200

    Fixed the gnome shell extension, now it works on gnome shell 3.24 and 3.26
    Fixed the install path when creating packages

--- multipackager.sh
+++ multipackager.sh
@@ -7,9 +7,9 @@ GTK_VERSION=`pkg-config --modversion gtk+-3.0`
 echo $GTK_VERSION
 if [ "$GTK_VERSION" \< "3.22" ];
 then
-	cmake .. -DGTK_3_20=on
+	cmake .. -DGTK_3_20=on -DCMAKE_INSTALL_PREFIX=/usr
 else
-	cmake ..
+	cmake .. -DCMAKE_INSTALL_PREFIX=/usr
 fi
-make
-make DESTDIR=$1 PREFIX=/usr install
+make VERBOSE=1
+make DESTDIR=$1 install
